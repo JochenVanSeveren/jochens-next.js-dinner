1fde8bc4e5f5593048fb81b764ba466a63b538d4
Jochen Van Severen <jochenvanseveren@outlook.com>
2023-06-19 03:51:07 +0200

    Messing around with the new html <dialog> element

--- app/recipes/edit/[slug]/RecipeForm.tsx
+++ app/recipes/edit/[slug]/RecipeForm.tsx
@@ -1,16 +1,18 @@
 "use client";
 
 import { Recipe as RecipeType } from "@prisma/client";
-import { use, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import urlSlug from "url-slug";
-import Image from "next/image";
 import { handleRecipeSubmit } from "@/lib/actions";
+import CldImage from "@/components/elements/CldImage";
 
 type RecipeFormProps = {
 	recipe: RecipeType | null;
 };
 
 export default function RecipeForm({ recipe }: RecipeFormProps) {
+	const [dialogIsOpen, setDialogIsOpen] = useState<boolean>(false);
+
 	const {} = recipe ?? {};
 	const [title, setTitle] = useState<string>(recipe?.title ?? "");
 	const [slug, setSlug] = useState<string>(recipe?.slug ?? "");
@@ -32,9 +34,19 @@ export default function RecipeForm({ recipe }: RecipeFormProps) {
 	// }
 
 	function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
-		// check if file is image
+		// check if files are present
 		if (!e.target.files) return;
-		// setImage(URL.createObjectURL(e.target.files[0]));
+
+		const file = e.target.files[0];
+		const MAX_SIZE = 20000000; // max size in bytes, e.g. 2MB
+		const permittedTypes = ["image/jpeg", "image/png"];
+
+		// check if file is an image with a permitted type and within size limit
+		if (file && permittedTypes.includes(file.type) && file.size <= MAX_SIZE) {
+			setImage(URL.createObjectURL(file));
+		} else {
+			setDialogIsOpen(true);
+		}
 	}
 
 	return (
@@ -49,18 +61,22 @@ export default function RecipeForm({ recipe }: RecipeFormProps) {
 					onChange={(e) => setTitle(e.target.value)}
 				/>
 				<p>Slug: {slug}</p>
-				{/* <Image
+				<CldImage
 					src={image || ""}
 					alt={"Upload image"}
 					width={500} // specify your desired width
 					height={300} // and height
-				/> */}
+				/>
 				<input
 					type="file"
 					name="image"
 					accept="image/*"
 					onChange={handleChange}
 				/>
+				<dialog open={dialogIsOpen}>
+					Please select an image (JPEG, PNG) smaller than 20MB
+					<button onClick={() => setDialogIsOpen(false)}>OK</button>
+				</dialog>
 				<button type="submit">Submit</button>
 			</form>
 		</>
