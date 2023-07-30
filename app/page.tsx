8b1ea5a2a654fa1095ccbce737f280e4cac518d1
Jochen Van Severen <jochenvanseveren@outlook.com>
2023-07-31 00:12:20 +0200

    Laptop design: inserted more flex 😎
    - Updated the structure of the Home page to use a fragment instead of a div
    - Adjusted the positioning and spacing of elements in the Home page
    - Added a footer with lock icon and password message in the Home page
    - Modified the layout of RecipePage to display ingredients and herbs side by side using flexbox
    - Altered the size of recipe images in RecipePage for better responsiveness
    - Changed the style of ordered list items in RecipePage to have custom numbering
    - Added CSS styles for custom ordered lists with bold, shadowed numbers

--- app/page.tsx
+++ app/page.tsx
@@ -8,62 +8,63 @@ import { HiLockClosed } from "react-icons/hi";
 
 export default function Home() {
 	return (
-		<div className="md:flex md:space-x-4 md:mx-4 md:mt-8 items-start">
-			<h3 className="text-left mb-8 mt-4 md:max-w-xl xl:max-w-2xl md:flex-1">
-				Met mijn darmaandoening (
-				<AuthCheck
-					permittedRoles={["USER", "ADMIN", "DEMO_ADMIN", "DEMO_USER"]}
-					showLockIcon={true}>
-					<a
-						className="text-buff-400 underline hover:text-buff-500"
-						href="https://www.gezondheidenwetenschap.be/richtlijnen/prikkelbaredarmsyndroom-pds"
-						target="_blank"
-						rel="noopener noreferrer">
-						pbs
-					</a>
-				</AuthCheck>
-				) krijg ik vaak volgende vragen
-			</h3>
-			<ul className="space-y-8 md:flex-1">
-				<li>
-					<Link href={"/cant-eats"} className="flex items-center space-x-2">
-						<button>
-							<AuthCheck
-								permittedRoles={["USER", "ADMIN", "DEMO_ADMIN", "DEMO_USER"]}
-								showLockIcon={true}>
-								<GiPoisonBottle />
-							</AuthCheck>
-						</button>
-						<span>Wat mag je niet eten?</span>
-					</Link>
-				</li>
-				<li>
-					<Link
-						href={"/likes"}
-						className="flex items-center space-x-2 mr-auto ml-auto">
-						<button>
-							<AuthCheck
-								permittedRoles={["USER", "ADMIN", "DEMO_ADMIN", "DEMO_USER"]}
-								showLockIcon={true}>
-								<BiLike />
-							</AuthCheck>
-						</button>{" "}
-						<span>Wat mag je dan wel eten?</span>
-					</Link>
-				</li>
-				<li>
-					<Link href={"/recipes"} className="flex items-center space-x-2">
-						<button>
-							<PiCookingPotBold />
-						</button>
-						<span>Wat maak jij dan zoal?</span>
-					</Link>
-				</li>
-			</ul>
-			<footer className="fixed bottom-2 flex items-center space-x-2">
+		<>
+			<div className="md:flex md:space-x-4 md:mx-4 md:mt-20 md-max-w-46 items-start">
+				<h3 className="text-left mb-8 mt-4 md:max-w-xl xl:max-w-2xl md:flex-1">
+					Met mijn darmaandoening (
+					<AuthCheck
+						permittedRoles={["USER", "ADMIN", "DEMO_ADMIN", "DEMO_USER"]}
+						showLockIcon={true}>
+						<a
+							className="text-buff-400 underline hover:text-buff-500"
+							href="https://www.gezondheidenwetenschap.be/richtlijnen/prikkelbaredarmsyndroom-pds"
+							target="_blank"
+							rel="noopener noreferrer">
+							pbs
+						</a>
+					</AuthCheck>
+					) krijg ik vaak volgende vragen
+				</h3>
+				<ul className="space-y-8 md:flex-1">
+					<li>
+						<Link href={"/cant-eats"} className="flex items-center space-x-2">
+							<button>
+								<AuthCheck
+									permittedRoles={["USER", "ADMIN", "DEMO_ADMIN", "DEMO_USER"]}
+									showLockIcon={true}>
+									<GiPoisonBottle />
+								</AuthCheck>
+							</button>
+							<span>Wat mag je niet eten?</span>
+						</Link>
+					</li>
+					<li>
+						<Link href={"/likes"} className="flex items-center space-x-2">
+							<button>
+								<AuthCheck
+									permittedRoles={["USER", "ADMIN", "DEMO_ADMIN", "DEMO_USER"]}
+									showLockIcon={true}>
+									<BiLike />
+								</AuthCheck>
+							</button>{" "}
+							<span>Wat mag je dan wel eten?</span>
+						</Link>
+					</li>
+					<li>
+						<Link href={"/recipes"} className="flex items-center space-x-2">
+							<button>
+								<PiCookingPotBold />
+							</button>
+							<span>Wat maak jij dan zoal?</span>
+						</Link>
+					</li>
+				</ul>
+			</div>
+
+			<footer className="fixed bottom-2 left-1/2 transform -translate-x-1/2 flex items-center space-x-2 whitespace-nowrap">
 				<HiLockClosed />
 				<p className="italic text-xs"> Enkel toegangelijk met wachtwoord</p>
 			</footer>
-		</div>
+		</>
 	);
 }
--- app/recipes/[slug]/page.tsx
+++ app/recipes/[slug]/page.tsx
@@ -23,59 +23,72 @@ export default async function RecipePage({ params }: Props) {
 
 	return (
 		<div className="space-y-8">
-			<h1 className="text-3xl mb-5 text-buff font-bold">{title}</h1>
-			<CldImage
-				src={image ?? ""}
-				alt={title}
-				width={600}
-				height={600}
-				// sizes="(max-width: 480px) 100vw, 50vw"
-				className="rounded-lg object-cover neumo ml-auto mr-auto"
-			/>
-			<div className="space-y-5">
-				<div>
-					<h2 className="text-2xl font-semibold space-x-1">Ingredients</h2>
-					<div className="flex flex-wrap">
-						{ingredients.map((ingredient) => (
-							<div key={ingredient} className="bg-buff p-2 rounded shadow-md">
-								{ingredient}
-							</div>
-						))}
-					</div>
+			<div className="flex flex-wrap justify-evenly space-x-4">
+				<div className="max-w-md">
+					<h1 className="text-3xl mb-5 text-buff font-bold">{title}</h1>
+					<CldImage
+						src={image ?? ""}
+						alt={title}
+						width={600}
+						height={500}
+						// sizes="(max-width: 480px) 100vw, 50vw"
+						className="rounded-lg object-cover neumo ml-auto mr-auto"
+					/>
 				</div>
-				{optionalIngredients.length > 0 && (
-					<div>
-						<h2 className="text-2xl font-semibold space-x-1">
-							Optional Ingredients
-						</h2>
-						<div className="flex flex-wrap">
-							{optionalIngredients.map((ingredient) => (
-								<div key={ingredient} className="bg-buff p-2 rounded shadow-md">
-									{ingredient}
-								</div>
-							))}
-						</div>
-					</div>
-				)}
-				<div>
-					<h2 className="text-2xl font-semibold space-x-1">Herbs</h2>
-					<div className="flex flex-wrap">
-						{herbs.map((herb) => (
-							<div key={herb} className="bg-buff p-2 rounded shadow-md">
-								{herb}
+				<div className="max-w-md">
+					<div className="space-y-5">
+						<div>
+							<h2 className="text-2xl font-semibold space-x-1">Ingredients</h2>
+							<div className="flex flex-wrap">
+								{ingredients.map((ingredient) => (
+									<div
+										key={ingredient}
+										className="bg-buff p-2 rounded shadow-md">
+										{ingredient}
+									</div>
+								))}
 							</div>
-						))}
+						</div>
+						{optionalIngredients.length > 0 && (
+							<div>
+								<h2 className="text-2xl font-semibold space-x-1">
+									Optional Ingredients
+								</h2>
+								<div className="flex flex-wrap">
+									{optionalIngredients.map((ingredient) => (
+										<div
+											key={ingredient}
+											className="bg-buff p-2 rounded shadow-md">
+											{ingredient}
+										</div>
+									))}
+								</div>
+							</div>
+						)}
+						<div>
+							<h2 className="text-2xl font-semibold space-x-1">Herbs</h2>
+							<div className="flex flex-wrap">
+								{herbs.map((herb) => (
+									<div key={herb} className="bg-buff p-2 rounded shadow-md">
+										{herb}
+									</div>
+								))}
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
 			<h2 className="text-2xl font-semibold">Steps</h2>
-			<ol className="list-decimal pl-5 space-y-2">
-				{steps.map((step) => (
-					<li key={step} className="text-lg">
-						{step}
-					</li>
-				))}
-			</ol>
+			<div className="custom-ordered-list">
+				<ul className="list-decimal pl-10 space-y-2 text-lg">
+					{steps.map((step, index) => (
+						<li key={index} className="mb-2 pl-2">
+							{step}
+						</li>
+					))}
+				</ul>
+			</div>
+
 			<AuthCheck permittedRoles={["ADMIN"]}>
 				<button>
 					<Link href={`/recipes/edit/${recipe.slug}`}>Edit</Link>
